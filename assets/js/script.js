1eb6645bab5bf8323591da19ca1616b57df8a85b
cpapanicholas <cpapanicholas@gmail.com>
2023-10-03 21:06:21 -0500

    added code to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
-var RukuZedTakaAPI = dd00af83e89105441c591b1fdc8aa109
+ RukuZedTakaAPI = dd00af83e89105441c591b1fdc8aa109
 
+document.addEventListener("DOMContentLoaded", () => {
+    const RukuZedTakaAPI = "dd00af83e89105441c591b1fdc8aa109 "; 
 
+    const searchButton = document.getElementById("search-button");
+    const cityInput = document.getElementById("city-input");
+    const weatherInfo = document.querySelector(".weather-info");
+    const searchHistoryList = document.getElementById("search-history-list");
