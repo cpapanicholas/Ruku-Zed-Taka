6c0461658df94819681cb57fde65fb613776eaf2
cpapanicholas <cpapanicholas@gmail.com>
2023-10-03 22:09:48 -0500

    added more js code and reoraganized it

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const cityInput = document.getElementById("city-input");
     const weatherInfo = document.querySelector(".weather-info");
     const searchHistoryList = document.getElementById("search-history-list");
+    const forecastList = document.getElementById("forecast-list");
 
-
+    
     function displayWeather(data) {
-        const city = data.name;
-        const temperature = Math.round((data.main.temp - 273.15) * 9/5 + 32);
-        const description = data.weather[0].description;
-        const humidity = data.main.humidity;
-        const windSpeed = data.wind.speed;
+        const {
+            name: city,
+            main: { temp, humidity },
+            weather: [{ description, icon }],
+            wind: { speed },
+            dt
+        } = data;
 
-        const date = new Date(data.dt * 1000);
+        const temperature = Math.round((temp - 273.15) * 9/5 + 32); 
+        const date = new Date(dt * 1000);
         const dateStr = date.toLocaleDateString();
-        const iconCode = data.weather[0].icon;
-        const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`; 
+        const iconUrl = `http://openweathermap.org/img/wn/${icon}.png`; 
 
+        
         const cityCard = document.createElement("div");
         cityCard.classList.add("city-card");
         cityCard.innerHTML = `
@@ -29,32 +33,65 @@ document.addEventListener("DOMContentLoaded", () => {
             <p>Temperature: ${temperature}°F</p>
             <p>Description: ${description}</p>
             <p>Humidity: ${humidity}%</p>
-            <p>Wind Speed: ${windSpeed} m/s</p>
+            <p>Wind Speed: ${speed} m/s</p>
         `;
 
         weatherInfo.appendChild(cityCard);
     }
-    searchButton.addEventListener("click", () => {
-        const cities = cityInput.value.trim().split(","); 
-        if (cities.length === 0) return;
 
-        weatherInfo.innerHTML = ""; 
+    
+    function displayForecast(data) {
+        forecastList.innerHTML = "";
 
-        cities.forEach(city => {
-            const trimmedCity = city.trim();
-            if (trimmedCity === "") return;
+        data.list.forEach(item => {
+            const {
+                dt,
+                weather: [{ icon }],
+                main: { temp, humidity },
+                wind: { speed }
+            } = item;
+
+            const date = new Date(dt * 1000); 
+            const dateStr = date.toLocaleDateString();
+            const iconUrl = `http://openweathermap.org/img/wn/${icon}.png`; 
+            const temperature = Math.round((temp - 273.15) * 9/5 + 32); 
 
             
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${trimmedCity}&appid=${dd00af83e89105441c591b1fdc8aa109}`)
+            const forecastCard = document.createElement("div");
+            forecastCard.classList.add("forecast-card");
+            forecastCard.innerHTML = `
+                <p>Date: ${dateStr}</p>
+                <img src="${iconUrl}" alt="Forecast">
+                <p>Temperature: ${temperature}°F</p>
+                <p>Humidity: ${humidity}%</p>
+                <p>Wind Speed: ${speed} m/s</p>
+            `;
+
+            forecastList.appendChild(forecastCard);
+        });
+    }
+
+        
+        function displayCityWeather(city) {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${dd00af83e89105441c591b1fdc8aa109}`)
                 .then(response => response.json())
                 .then(data => {
                     displayWeather(data);
-                    addToSearchHistory(trimmedCity);
+                    addToSearchHistory(city);
                 })
                 .catch(error => {
                     console.error(error);
-                    weatherInfo.innerHTML += `<p>Weather data for ${trimmedCity} not found.</p>`;
+                    weatherInfo.innerHTML = "City not found.";
                 });
-        });
-    });
-});
+    
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${dd00af83e89105441c591b1fdc8aa109}`)
+                .then(response => response.json())
+                .then(data => {
+                    displayForecast(data);
+                })
+                .catch(error => {
+                    console.error(error);
+                    forecastList.innerHTML = "Forecast data not found.";
+                });
+        }
+    
